0c3a379a2c3d1b9bddd35f03912abbc2cd84df8a
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-05-30 21:15:33 -0400

    Add analysis plot script

---
+++ analysis/analysis_plot.py
@@ -0,0 +1,61 @@
+from data import LidarDataClient
+
+import matplotlib.pyplot as plt
+import math
+import os
+import time
+
+HOST = 'localhost'   # The remote host
+PORT = 8080              # The same port as used by the server
+
+# Create figure:
+fig1 = plt.figure(1)
+
+# Clear figure:
+plt.clf()
+
+# Enable interactive mode:
+plt.ion()
+
+# Create polar subplot:
+sbp = plt.subplot(111)
+
+sbp.autoscale(True)
+
+# Create axis:
+ax = fig1.add_axes(sbp)
+
+# Set radius limit:
+ax.set_ylim(ymin=-100, ymax=500)
+ax.set_xlim(xmin=-400, xmax=400)
+
+
+def unzip_catch(d):
+    try:
+        x, y = zip(*d)
+    except ValueError:
+        x = y = []
+    return x, y
+
+if __name__ == "__main__":
+    with LidarDataClient(HOST, PORT) as s:
+        # Initial plot:
+        plt.plot([0], [0], marker='.', color='black')
+        wnt_pts, = plt.plot([], [], lw=1, marker='+', color='r')
+        otr_pts, = plt.plot([], [], lw=1, marker='+', color='b')
+
+        while True:
+            # Update plot data:
+            wnt_x, wnt_y = unzip_catch(s.get_wnt_data())
+            otr_x, otr_y = unzip_catch(s.get_otr_data())
+
+            wnt_pts.set_xdata(wnt_x)
+            wnt_pts.set_ydata(wnt_y)
+            otr_pts.set_xdata(otr_x)
+            otr_pts.set_ydata(otr_y)
+
+            # Draw new plot:
+            plt.draw()
+
+            # Pause for plot update:
+            plt.pause(0.01)
